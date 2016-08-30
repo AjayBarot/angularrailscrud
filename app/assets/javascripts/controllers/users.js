84bf0b70fcc318a568580c9ffaee2137dacae780
ajaybarot <abarot@plumslice.com>
2016-08-30 15:17:04 +0530

    crud

---
+++ app/assets/javascripts/controllers/users.js
@@ -0,0 +1,75 @@
+var myApp = angular.module('angularCrud', ['ngRoute', 'ngResource', 'ui.bootstrap', 'templates']); 
+
+myApp.factory('Users', ['$resource',function($resource){
+  return $resource('/users.json', {},{
+    query: { method: 'GET', isArray: true },
+    create: { method: 'POST' }
+  })
+}]);
+
+myApp.factory('User', ['$resource', function($resource){
+  return $resource('/users/:id.json', {}, {
+    show: { method: 'GET' },
+    update: { method: 'PUT', params: {id: '@id'} },
+    delete: { method: 'DELETE', params: {id: '@id'} }
+  });
+}]);
+
+//Controller
+myApp.controller("UserListCtr", ['$scope', '$http', '$resource', 'Users', 'User', '$location', function($scope, $http, $resource, Users, User, $location) {
+  $scope.users = Users.query();
+  $scope.deleteUser = function (userId) {
+    if (confirm("Are you sure you want to delete this user?")){
+      User.delete({ id: userId }, function(){
+        $scope.users = Users.query();
+        $location.path('/');
+      });
+    }
+  };
+}]);
+
+myApp.controller("UserUpdateCtr", ['$scope', '$resource', 'User', '$location', '$routeParams', function($scope, $resource, User, $location, $routeParams) {
+  $scope.user = User.get({id: $routeParams.id})
+  $scope.update = function(){
+    if ($scope.userForm.$valid){
+      User.update({id: $scope.user.id},{user: $scope.user},function(){
+        $location.path('/');
+      }, function(error) {
+        console.log(error)
+      });
+    }
+  };
+}]);
+
+myApp.controller("UserAddCtr", ['$scope', '$resource', 'Users', '$location', function($scope, $resource, Users, $location) {
+  $scope.user = {addresses: [{street1: '', street2: '', city: '', state: '', country: '', zipcode: '' }]}
+  $scope.save = function () {
+    if ($scope.userForm.$valid){
+      Users.create({user: $scope.user}, function(){
+        $location.path('/');
+      }, function(error){
+        console.log(error)
+      });
+    }
+  }
+}]);
+
+myApp.config([
+  '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/users',{
+      templateUrl: 'index.html',
+      controller: 'UserListCtr'
+    });
+    $routeProvider.when('/users/new', {
+      templateUrl: 'new.html',
+      controller: 'UserAddCtr'
+    });
+    $routeProvider.when('/users/:id/edit', {
+      templateUrl: 'edit.html',
+      controller: "UserUpdateCtr"
+    });
+    $routeProvider.otherwise({
+      redirectTo: '/users'
+    });
+  }
+]);
